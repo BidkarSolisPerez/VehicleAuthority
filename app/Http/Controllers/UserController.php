ed40a90efa00207668bb75e1e34aaf5a2e933dcf
BidkarSolisPerez <bidkr.solis@gmail.com>
2018-11-07 21:21:31 -0600

    Agregando autenticacion de usarios
    Agregando autenticación de usuarios y manejo de session

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,9 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use DB;
+use Session;
 
 class UserController extends BaseController
 {
@@ -16,7 +19,36 @@ class UserController extends BaseController
         $username = $req->input('username');
         $password = $req->input('password');
 
-        echo $username . "----" .$password;
+        //echo $username . "----" .$password;
     
+        $checkLogin = DB::table('login')->where(['user_name'=>$username,'password'=>$password])->get();
+
+        if(count($checkLogin) > 0){
+            
+            echo "User found";
+            session(['currentUser'=>$username]);
+            return redirect('ServiceRequestView');
+            echo "Nombre del usuario es " . Session::get('currentUser') . "</br>";
+            session()->forget('currentUser');
+            if(session()->has('currentUser')){
+                echo "Nombre del usuario es " . Session::get('currentUser') . "</br>";
+            }else{
+                echo "User was removed from session</br>";
+            }
+            
+
+            //In order to check if session has a value with key
+            /*
+                if(session()->has('currentUser')){
+
+                }else{
+
+                }
+            */
+
+        }else{
+            echo "User did not found";
+        }
+
     }
 }
--- resources/views/Pages/serviceRequestView.blade.php
+++ resources/views/Pages/serviceRequestView.blade.php
@@ -2,6 +2,10 @@
 <html>
 <head>
     <title>Pantalla de vista de los solicitudes de servicio</title>
+
+    <?php
+         echo "Nombre del usuario es " . Session::get('currentUser') . "</br>";
+    ?>
 </head>
 <body>
 <h1>Bienendo a la pantalla de solicitude de servicio</h1>
